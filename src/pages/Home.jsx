6060facf32280f0a213582de94ccd798566fa016
31pablo05 <Pabloproboste64@gmail.com>
2025-01-30 10:01:13 -0300

    prueba 2 despliege con img

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -28,7 +28,7 @@ const Home = () => {
                 {/* Imagen del profesional */}
                 <div className="md:w-1/2 flex justify-center">
                     <img
-                        src="/src/assets/webp/profesional.webp"
+                        src="/public/assets/webp/profesional.webp"
                         alt="Francisco Torres, osteópata profesional"
                         className="rounded-lg shadow-lg w-full max-w-lg object-cover"
                     />
