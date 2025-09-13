931bc580e5fd6b9a894a418c56bce84317441e68
31pablo05 <Pabloproboste64@gmail.com>
2025-09-12 22:34:29 -0300

    icono de scroll eliminado

--- src/components/home/HeroSection.jsx
+++ src/components/home/HeroSection.jsx
@@ -205,16 +205,7 @@ const HeroSection = () => {
         </div>
       </motion.div>
 
-      {/* Scroll indicator */}
-      <motion.div
-        className="absolute bottom-8 left-1/2 transform -translate-x-1/2"
-        animate={{ y: [0, 10, 0] }}
-        transition={{ duration: 2, repeat: Infinity }}
-      >
-        <div className="w-6 h-10 border-2 border-white/50 rounded-full flex justify-center">
-          <div className="w-1 h-3 bg-white/50 rounded-full mt-2" />
-        </div>
-      </motion.div>
+     
     </section>
   );
 };
